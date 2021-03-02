73fc9cec6875a3613d8e3e76ff0c5f5a41f08a92
Deepanshu Sharma <46198996+Deepanshus98@users.noreply.github.com>
2021-03-02 14:20:10 +0530

    Update clonegraph.py

--- clonegraph.py
+++ clonegraph.py
@@ -1,26 +1,23 @@
-"""
-# Definition for a Node.
-class Node(object):
-    def __init__(self, val, neighbors):
-        self.val = val
-        self.neighbors = neighbors
-"""
-class Solution(object):
-    def cloneGraph(self, node):
-        """
-        :type node: Node
-        :rtype: Node
-        """
-        node_copy = self.dfs(node, dict())
-        return node_copy
-    
-    def dfs(self, node, hashd):
-        if not node: return None
-        if node in hashd: return hashd[node]
-        node_copy = Node(node.val, [])
-        hashd[node] = node_copy
-        for n in node.neighbors:
-            n_copy = self.dfs(n, hashd)
-            if n_copy:
-                node_copy.neighbors.append(n_copy)
-        return node_copy
+
+def cloneGraph(self, node: 'Node') -> 'Node':
+        if node == None:
+            return None
+        
+        vi = set()
+        hm = {}
+        
+        def dfs(node, vi, hm):
+            vi.add(node)
+            copyNode = Node(node.val, [])
+            hm[node] = copyNode
+            
+            for dest in node.neighbors:
+                if dest not in vi:
+                    dfs(dest, vi, hm)
+                
+                copyNode.neighbors.append(hm[dest])
+        
+        dfs(node, vi, hm)
+        
+        return hm[node]
+"
